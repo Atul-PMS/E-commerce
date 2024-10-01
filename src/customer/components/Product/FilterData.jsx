e49925ef0041333b5a8d76f156ec32a39169e46e
Atul-PMS <atul.kumar.psa@postmortemshala.co.in>
2024-10-01 12:41:46 +0530

    E-commerce .

---
+++ src/customer/components/Product/FilterData.jsx
@@ -0,0 +1,76 @@
+export const color = [
+    "whtie",
+    "Black",
+    "Red",
+    "marun",
+    "Being",
+    "Pink",
+    "Green",
+    "Yellow",
+];
+
+export const filters = [
+    {
+        id: "color",
+        name: "color",
+        options: [
+            {value: "white", label: "white"},
+            {value: "Black", label: "Black"},
+            {value: "Red", label: "Red"},
+            {value: "marun", label: "marun"},
+            {value: "Being", label: "Being"},
+            {value: "Pink", label: "Pink"},
+            {value: "Green", label: "Green"},
+            {value: "Yellow", label: "Yellow"},
+        ],
+    },
+    {
+        id: "size",
+        name: "size",
+        options: [
+            {value: "S", label: "S"},
+            {value: "M", label: "M"},
+            {value: "L", label: "L"},
+            {value: "XL", label: "XL"},
+        ],
+    },
+
+];
+
+export const sinlefilter = [
+    {
+        id: "price",
+        name: "price",
+        options: [
+            {value: "159-399", label: "₹159-₹399"},
+            {value: "399-999", label: "₹399-₹999"},
+            {value: "999-1999", label: "₹999-₹1999"},
+            {value: "1999-2999", label: "₹1999-₹2999"},
+            {value: "2999-3999", label: "₹2999-₹3999"},
+           
+        ],
+    },
+    {
+        id: "disccout",
+        name: "disccout Range",
+        options: [
+            {value: "20", label: "20% And Above"},
+            {value: "30", label: "30% And Above"},
+            {value: "40", label: "40% And Above"},
+            {value: "50", label: "50% And Above"},
+            {value: "60", label: "60% And Above"},
+            {value: "70", label: "70% And Above"},
+            {value: "80", label: "80% And Above"},
+        ],
+    },
+    {
+        id: "stock",
+        name: "Availability",
+        options: [
+            {value: "in_stock", label: "In Stock"},
+            {value: "out_of_stock", label: "out of Stock"},
+            
+        ],
+    },
+ 
+    ]
